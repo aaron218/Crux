449ba4a9018d414e06109325238871e2be6d90a0
aaron218 <12155829@qq.com>
2015-10-25 23:08:11 +0800

    update

---
+++ crux-jclouds/crux-OpenStack/src/main/java/net/newString/crux/OpenStack/ConstantValue.java
@@ -0,0 +1,7 @@
+package net.newString.crux.OpenStack;
+
+/**
+ * Created by Administrator on 2015/10/25 0025.
+ */
+public class ConstantValue {
+}
---
+++ crux-jclouds/crux-OpenStack/src/test/java/net/newString/crux/OpenStack/ConstantValueTest.java
@@ -0,0 +1,10 @@
+package net.newString.crux.OpenStack;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2015/10/25 0025.
+ */
+public class ConstantValueTest {
+
+}
