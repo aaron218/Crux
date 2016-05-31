434ae269803967d5473287228ca5fc33053848e2
aaron218 <12155829@qq.com>
2016-05-31 23:25:50 +0800

    update

---
+++ crux-extend/src/test/java/net/newString/crux/extend/Simulation/SimCarsTest.java
@@ -0,0 +1,8 @@
+package net.newString.crux.extend.Simulation;
+
+/**
+ * Created by Administrator on 2016/5/31 0031.
+ */
+public class SimCarsTest {
+
+}
---
+++ crux-search/src/main/java/net/newString/crux/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by Administrator on 2016/5/31 0031.
+ */
+package net.newString.crux.search;
